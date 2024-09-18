ed6aa68f548cd1389a2b94f5e38a0e9cfbeeafba
Shazeel Ali <75409190+ShazeelAli@users.noreply.github.com>
2024-09-18 20:28:38 +1200

    Finished Receipt Room, some bugs to clean up, bounding the receipt movement to the left hand side scene rather than letting it move over the menu

--- src/main/java/nz/ac/auckland/se206/controllers/ReceiptController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ReceiptController.java
@@ -1,13 +1,21 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import nz.ac.auckland.se206.GameStateContext;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class ReceiptController extends Controller {
+  private static GameStateContext context = new GameStateContext();
   @FXML private ImageView receiptImageView;
 
   @FXML private ImageView receiptPiece1x1;
@@ -28,6 +36,15 @@ public class ReceiptController extends Controller {
   private ImageView dragTarget;
   private double dragStartMouseX = 0;
   private double dragStartMouseY = 0;
+
+  // chat-room
+  @FXML private Label timerLabel; //
+  @FXML private Button btnCrimeScene;
+  @FXML private Button btnJimmy;
+  @FXML private Button btnGrandma;
+  @FXML private Button btnBusinessman;
+  @FXML private Button btnGuess;
+  @FXML private TextArea txtaChat;
 
   @FXML
   public void initialize() {
@@ -134,13 +151,90 @@ public class ReceiptController extends Controller {
   }
 
   private void onPuzzleSolve() {
-    for (int i = 0; i < 2; i++) {
-      for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 2; j++) {
         receiptPieces[i][j].setDisable(true);
         receiptPieces[i][j].setOpacity(0);
       }
     }
     receiptImageView.setOpacity(100);
     return;
+  }
+
+  /** when switched to disable button */
+  @Override
+  public void onSwitchTo() {
+    btnGuess.setDisable(true);
+    if (BusinessmanController.businessmanChatted == true
+        && GrandmaController.grandmaChatted == true
+        && JimmyController.jimmyChatted == true) {
+      btnGuess.setDisable(false);
+    }
+  }
+
+  /**
+   * Handles the switch button click event to jimmy's scene.
+   *
+   * @param event the action event triggered by clicking the guess button
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void handleCrimeSceneClick(ActionEvent event) throws IOException {
+    SceneManager.switchRoot(AppUi.MAIN_ROOM);
+    txtaChat.clear();
+  }
+
+  @FXML
+  private void handleJimmyClick(ActionEvent event) throws IOException {
+    JimmyController jimmyController =
+        (JimmyController) SceneManager.getController(AppUi.JIMMY_ROOM);
+    SceneManager.switchRoot(AppUi.JIMMY_ROOM);
+    jimmyController.startChat();
+    txtaChat.clear();
+  }
+
+  /**
+   * Handles the switch button click event to grandma's scene.
+   *
+   * @param event the action event triggered by clicking the guess button
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void handleGrandmaClick(ActionEvent event) throws IOException {
+    GrandmaController grandmaController =
+        (GrandmaController) SceneManager.getController(AppUi.GRANDMA_ROOM);
+    SceneManager.switchRoot(AppUi.GRANDMA_ROOM);
+    grandmaController.startChat();
+    txtaChat.clear();
+  }
+
+  /**
+   * Handles the switch button click event to grandma's scene.
+   *
+   * @param event the action event triggered by clicking the guess button
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void handleBusinessmanClick(ActionEvent event) throws IOException {
+    BusinessmanController businessmanController =
+        (BusinessmanController) SceneManager.getController(AppUi.BUSINESSMAN_ROOM);
+    SceneManager.switchRoot(AppUi.BUSINESSMAN_ROOM);
+    businessmanController.startChat();
+    txtaChat.clear();
+  }
+
+  /**
+   * Handles the guess button click event.
+   *
+   * @param event the action event triggered by clicking the guess button
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void handleGuessClick(ActionEvent event) throws IOException {
+    GuessingController guessingController =
+        (GuessingController) SceneManager.getController(AppUi.GUESSING_ROOM);
+    SceneManager.switchRoot(AppUi.GUESSING_ROOM);
+    guessingController.startChat();
+    context.handleGuessClick();
   }
 }
